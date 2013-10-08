27d5d19b97830b919fc284979c7823f3f2386383
Hans-Christoph Steiner <hans@eds.org>
2013-10-07 22:07:22 -0400

    the package name must remain PyMTP, pypi doesn't allow name changes
    If named 'pymtp', it will just fail to upload with this error:
    Upload failed (401): You must be identified to edit package information

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import os
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-setup(name = "pymtp",
+setup(name = "PyMTP",
       version = "0.1.0",
       description = "LibMTP bindings in Python",
       long_description=read('README'),
