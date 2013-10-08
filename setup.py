b7f5302ab907b4a0709e9c41d89e3ebc35475c6e
Hans-Christoph Steiner <hans@eds.org>
2013-10-07 20:58:43 -0400

    downcase package name since that is the standard way these days

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # 
-# Setup.py for Pymtp
+# Setup.py for pymtp
 #
 
 from distutils.core import setup
 
-setup(name = "PyMTP",
+setup(name = "pymtp",
       version = "0.0.5",
       description = "LibMTP bindings in Python",
       author = "Nick Devito",
       author_email = "nick@nick125.com",
-      url = "https://pypi.python.org/pypi/PyMTP",
+      url = "https://pypi.python.org/pypi/pymtp",
       py_modules = ["pymtp"])
