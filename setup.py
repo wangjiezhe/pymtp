aa266f0b568ee22e9f92f2cca8923f1548b54219
Hans-Christoph Steiner <hans@eds.org>
2013-10-07 21:05:06 -0400

    add README as long_description in setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,15 @@
 #
 
 from distutils.core import setup
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name = "PyMTP",
       version = "0.1.0",
       description = "LibMTP bindings in Python",
+      long_description=read('README'),
       author = "Nick Devito",
       author_email = "nick@nick125.com",
       url = "https://pypi.python.org/pypi/PyMTP",
